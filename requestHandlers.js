623c96486881d4e1d1201c2f0d71cbc400790e5e
RinaCoimbra <rinacoimbra@gmail.com>
2016-01-19 14:23:09 -0200

    mais coisa talvez

---
+++ requestHandlers.js
@@ -0,0 +1,112 @@
+var querystring = require("querystring"),
+    fs = require("fs"),
+    formidable = require("formidable");
+var http = require("https");
+
+function start(response) {
+  console.log("Request handler 'start' was called.");
+
+  var body = '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" '+
+    'content="text/html; charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/upload" enctype="multipart/form-data" '+
+    'method="post">'+
+    '<input type="file" name="upload" multiple="multiple">'+
+    '<input type="submit" value="Upload file" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
+
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(body);
+    response.end();
+}
+
+function upload(response, request) {
+  console.log("Request handler 'upload' was called.");
+
+  var form = new formidable.IncomingForm();
+  console.log("about to parse");
+  form.parse(request, function(error, fields, files) {
+    console.log("parsing done");
+
+    /* Possible error on Windows systems:
+       tried to rename to an already existing file */
+    fs.rename(files.upload.path, "/tmp/test.png", function(err) {
+      if (err) {
+        fs.unlink("/tmp/test.png");
+        fs.rename(files.upload.path, "/tmp/test.png");
+      }
+    });
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("received image:<br/>");
+    response.write("<img src='/show' />");
+    response.end();
+  });
+}
+
+function show(response) {
+  console.log("Request handler 'show' was called.");
+  response.writeHead(200, {"Content-Type": "image/png"});
+  fs.createReadStream("/tmp/test.png").pipe(response);
+}
+
+function initialPokemon(response) {
+  var pkm = ["Charmander", "Squirtle", "Bulbasaur"];
+  var digimon = {"nome": "agumon", "evolucoes": ["greymon", "wargreymon"]};
+  response.writeHead(200, {"Content-Type": "text/json"});
+  response.write(JSON.stringify(digimon));
+  response.end();
+}
+
+function getUrl(url, response2, callback){
+  var request = http.get(url, function (response) {
+    // data is streamed in chunks from the server
+    // so we have to handle the "data" event    
+    var buffer = "", 
+        data,
+        route;
+
+    response.on("data", function (chunk) {
+        buffer += chunk;
+    }); 
+
+    response.on("end", function (err) {
+        // finished transferring data
+        // dump the raw data
+        console.log(buffer);
+        console.log("\n");
+        data = JSON.parse(buffer);
+        callback(data, response2);
+    }); 
+}); 
+}
+
+function returnLeagueData(data, response){
+  response.writeHead(200, {"Content-Type": "text/json"});
+  response.write(JSON.stringify(data));
+  response.end();
+}
+
+function myLeagueData(response){
+  var url = "https://na.api.pvp.net/api/lol/br/v1.4/summoner/by-name/RinaLovelace?api_key=7c20378c-001e-4639-ab96-669be9f17f7f";
+  getUrl(url, response, returnLeagueData);
+}
+
+function pagepage(response){
+  fs.readFile('helloworld.html',function (err, data){
+        response.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+        response.write(data);
+        response.end();
+    });
+}
+
+exports.start = start;
+exports.upload = upload;
+exports.show = show;
+exports.initialPokemon = initialPokemon;
+exports.myLeagueData = myLeagueData;
+exports.pagepage = pagepage;
---
+++ server.js
@@ -0,0 +1,17 @@
+var http = require("http");
+var url = require("url");
+
+var port = process.env.PORT || 5000;
+
+function start(route, handle) {
+  function onRequest(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received.");
+    route(handle, pathname, response, request);
+  }
+
+  http.createServer(onRequest).listen(port);
+  console.log("Server has started.");
+}
+
+exports.start = start;
