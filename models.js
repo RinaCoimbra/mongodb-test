069af320e0a18be55bd2e73d5bed9001bc1a9d6a
RinaCoimbra <rinacoimbra@gmail.com>
2016-01-20 08:04:59 -0200

    Models for Heroku Mongo

--- models.js
+++ models.js
@@ -1,9 +1,30 @@
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
-var url = "process.env.MONGOLAB_URI" || "mongodb://localhost/test";
+var url = process.env.MONGOLAB_URI || "mongodb://localhost/test";
 
 
+var restaurant_1 = {
+  "address": {
+    "street": "2 Avenue",
+    "zipcode": "10075",
+    "building": "1480",
+    "coord": [-73.9557413, 40.7720266]
+  },
+  "borough": "Manhattan",
+  "cuisine": "Italian",
+  "grades": [{
+    "date": new Date("2014-10-01T00:00:00Z"),
+    "grade": "A",
+    "score": 11
+  }, {
+    "date": new Date("2014-01-16T00:00:00Z"),
+    "grade": "B",
+    "score": 17
+  }],
+  "name": "Vella",
+  "restaurant_id": "41704620"
+};
 
 var insertDocument = function(db, callback) {
   db.collection('AlunoHogwarts').insertOne({
