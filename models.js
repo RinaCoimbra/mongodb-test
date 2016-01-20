44bf3371c5f57627d6fa43391e59f312c5113aea
RinaCoimbra <rinacoimbra@gmail.com>
2016-01-20 07:32:48 -0200

    Hogwarts

--- models.js
+++ models.js
@@ -3,44 +3,31 @@ var assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
 var url = "mongodb://heroku_3h7dws7z:cbedvslhinh4b2c3okque48l5l@ds047355.mongolab.com:47355/heroku_3h7dws7z";
 
+
+
 var insertDocument = function(db, callback) {
-  db.collection('restaurants').insertOne({
-    "address": {
-      "street": "2 Avenue",
-      "zipcode": "10075",
-      "building": "1480",
-      "coord": [-73.9557413, 40.7720266]
-    },
-    "borough": "Manhattan",
-    "cuisine": "Italian",
-    "grades": [{
-      "date": new Date("2014-10-01T00:00:00Z"),
-      "grade": "A",
-      "score": 11
-    }, {
-      "date": new Date("2014-01-16T00:00:00Z"),
-      "grade": "B",
-      "score": 17
-    }],
-    "name": "Vella",
-    "restaurant_id": "41704620"
+  db.collection('AlunoHogwarts').insertOne({
+    "Nome": "Harry Potter",
+    "Casa": "Grifinória",
+    "Mae": "Lilian Potter",
+    "Pai": "James Potter",
   }, function(err, result) {
     assert.equal(err, null);
-    console.log("Inserted a document into the restaurants collection.");
+    console.log("Inserted a document into the AlunoHogwarts collection.");
     callback(result);
   });
 };
 
 var findRestaurants = function(db, callback) {
-   var cursor =db.collection('restaurants').find( );
-   cursor.each(function(err, doc) {
-      assert.equal(err, null);
-      if (doc != null) {
-         console.dir(doc);
-      } else {
-         callback();
-      }
-   });
+  var cursor = db.collection('restaurants').find();
+  cursor.each(function(err, doc) {
+    assert.equal(err, null);
+    if (doc != null) {
+      console.dir(doc);
+    } else {
+      callback();
+    }
+  });
 };
 
 function insertDocumentIntoMongo() {
